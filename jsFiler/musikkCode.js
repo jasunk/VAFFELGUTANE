d2ff54ab43d131b04425d492e4da5b362fa96602
jasunk <jskraakenes@gmail.com>
2022-11-01 16:59:19 +0100

    la musikk inn i malen

--- jsFiler/musikkCode.js
+++ jsFiler/musikkCode.js
@@ -21,6 +21,7 @@ function musikk(){
 setInterval(duration, 1000)
 
 document.getElementById("albumBilde").src = currentSong[1]
+document.getElementById("albumBilde").style.borderRadius="50px"
 document.getElementById("songName").innerHTML = currentSong[0] + " - VAFFELGUTANE"
 
 function duration(){
@@ -88,4 +89,42 @@ function hoho(){
         playing=true
         audio.play();
     }
+}
+
+
+
+
+
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.querySelector(".blob").style.width = "33vw";
+        document.querySelector(".blob").style.height = "33vw";
+        document.querySelector(".blob").style.top = "-15vw";
+        document.querySelector(".blob").style.left = "-15vw";
+        document.querySelector("#blob2").style.width = "43vw";
+        document.querySelector("#blob2").style.height = "43vw";
+        document.querySelector("#blob2").style.top = "-15vw";
+        document.querySelector("#blob2").style.left = "-15vw";
+        document.querySelector("#dining-icon").style.top="2.66vw";
+        document.querySelector("#dining-icon").style.left="2.66vw";
+        document.querySelector("#dining-icon").style.fontSize="12vw";
+        document.querySelector(".top-bar").style.height="7vw";
+    } 
+    else {
+        document.querySelector(".blob").style.height = "26vw";
+        document.querySelector(".blob").style.height = "26vw";
+        document.querySelector(".blob").style.top = "-7vw";
+        document.querySelector("#blob2").style.height = "31vw";
+        document.querySelector("#blob2").style.width = "31vw";
+        document.querySelector("#blob2").style.top = "-7vw";
+        document.querySelector("#blob2").style.left = "-7vw";
+        document.querySelector("#dining-icon").style.top="3vw"
+        document.querySelector("#dining-icon").style.left="3vw";
+        document.querySelector("#dining-icon").style.fontSize="5vw";
+        document.querySelector(".top-bar").style.height="4vw";
+    }
+  prevScrollpos = currentScrollPos;
 }
