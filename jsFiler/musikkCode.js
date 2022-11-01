5a2934eedfa5d7efcbf512a183cb9c48f049adfd
jasunk <jskraakenes@gmail.com>
2022-11-01 16:30:54 +0100

    generell musikk
    la til audio.pause()
    fikset audio.start()
    La til rulering i valg av låter
    fikset valg i låter generelt
    big D, top G

--- jsFiler/musikkCode.js
+++ jsFiler/musikkCode.js
@@ -5,13 +5,18 @@ const song3 = ["Du er kul", "bilder/musikk/eirikAlbum.jpg", "621", "lydfiler/rus
 
 let currentSong = song2
 
-var audio = new Audio(currentSong[3]); audio.play();
+var audio = new Audio(currentSong[3]); 
+
 let durationNR = parseInt(currentSong[2]) 
 let durationMinus = 100/(durationNR-1)
 let durationWidth = 0
 
 let playing=true
 
+setInterval(musikk,10)
+function musikk(){
+    audio.play();
+}
 
 setInterval(duration, 1000)
 
@@ -19,34 +24,50 @@ document.getElementById("albumBilde").src = currentSong[1]
 document.getElementById("songName").innerHTML = currentSong[0] + " - VAFFELGUTANE"
 
 function duration(){
+    if (playing){
     durationNR -= 1
     durationWidth+=durationMinus
     console.log(durationNR + durationMinus, durationWidth)
     
 
-    document.getElementById("durationTime").innerHTML = parseInt(durationNR).toString()
+    
     
     if (durationWidth<100+durationMinus){
         document.getElementById("duration").style.width = durationWidth + "%"
     }
+}
     
     
 }
 
 function skip(){
+    audio.pause()
+    if (currentSong==song1){
+        currentSong = song2
+    }else if(currentSong==song2){
+        currentSong=song3
+    }else if(currentSong==song3){
+        currentSong=song1
+    }
     
-    currentSong = song3
     infoReset()
 }
 function back(){
-    currentSong = song1
+    audio.pause()
+    if (currentSong==song3){
+        currentSong = song2
+    }else if(currentSong==song2){
+        currentSong=song1
+    }else if(currentSong==song1){
+        currentSong=song3
+    }
     infoReset()
 }
 
 
 function infoReset(){
-    audio.pause()
-    var audio = new Audio(currentSong[3]); audio.play();
+
+    audio = new Audio(currentSong[3]); audio.play();
 
     durationNR = parseInt(currentSong[2]) 
     durationMinus = 100/(durationNR-1)
@@ -57,10 +78,14 @@ function infoReset(){
    
 }
 
-function pause(){
+function hoho(){
     if (playing){
+        
         playing=false
+        audio.pause();
     }else{
+        
         playing=true
+        audio.play();
     }
 }
